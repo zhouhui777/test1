b037a845da76120a6b50882c2c7eec4141197f66
coueow <coueow@DESKTOP-AB230QJ>
2020-02-07 15:55:24 +0800

    55

--- test1/src/test1/test.java
+++ test1/src/test1/test.java
@@ -3,14 +3,11 @@ package test1;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println(1);
-		System.out.println(2);
+		
+		
 		System.out.println("真尼玛无聊");
 		System.out.println(555);
 
-		//666666
-
-                //555555555555
-		//555
+		
 	}
 }
